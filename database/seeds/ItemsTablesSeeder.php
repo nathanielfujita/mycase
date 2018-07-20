66ced6a8ec4a2d6b2a4f41145806430f17d15cf1
EC2 Default User <ec2-user@ip-172-31-2-147.us-east-2.compute.internal>
2018-07-20 03:44:42 +0000

    added images

--- database/seeds/ItemsTablesSeeder.php
+++ database/seeds/ItemsTablesSeeder.php
@@ -11,8 +11,393 @@ class ItemsTablesSeeder extends Seeder
      */
     public function run()
     {
-       //
         
+$param = [
+            'image' => 'image/f_id_1.jpg',
+            'sex' => 'F',
+            'description'=>'夏らしいレーストップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
 
+$param = [
+            'image' => 'image/f_id_2.jpg',
+            'sex' => 'F',
+            'description'=>'ボーダー',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_3.jpg',
+            'sex' => 'F',
+            'description'=>'エメラルドグリーンの花柄トップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_4.jpg',
+            'sex' => 'F',
+            'description'=>'マスタード色ショートスリーブ',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_5.jpg',
+            'sex' => 'F',
+            'description'=>'MARVEL Tシャツ',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_6.jpg',
+            'sex' => 'F',
+            'description'=>'エメラルドグリーンレース　トップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_7jpg',
+            'sex' => 'F',
+            'description'=>'ギンガムチェックのトップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_8.jpg',
+            'sex' => 'F',
+            'description'=>'ブラック　トップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_9.jpg',
+            'sex' => 'F',
+            'description'=>'グレーの長袖トップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_10.jpg',
+            'sex' => 'F',
+            'description'=>''赤ビスチェ,
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_11.jpg',
+            'sex' => 'F',
+            'description'=>'バーガンディの花柄トップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_12.jpg',
+            'sex' => 'F',
+            'description'=>'白　長袖のトップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_13.jpg',
+            'sex' => 'F',
+            'description'=>'シンプルな白のトップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_14.jpg',
+            'sex' => 'F',
+            'description'=>'白　シースルートップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_15.jpg',
+            'sex' => 'F',
+            'description'=>'白のブラウス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_16.jpg',
+            'sex' => 'F',
+            'description'=>'エスニック風白トップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_17.jpg',
+            'sex' => 'F',
+            'description'=>'黒のトップス',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_18.jpg',
+            'sex' => 'F',
+            'description'=>'ノースリーブ花柄レース',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_19.jpg',
+            'sex' => 'F',
+            'description'=>'エスニック風総柄ワンピース',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_20.jpg',
+            'sex' => 'F',
+            'description'=>'ボタニカルワンピース',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_21.jpg',
+            'sex' => 'F',
+            'description'=>'マーメイドワンピース',
+            'name' => 'tops',
+            ];
+
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/f_id_22.jpg',
+            'sex' => 'F',
+            'description'=>'赤スカート',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+            
+$param = [
+            'image' => 'image/f_id_23.jpg',
+            'sex' => 'F',
+            'description'=>'白の花柄ワンピース',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_24.jpg',
+            'sex' => 'F',
+            'description'=>'ピンク黒チェックスカート',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);    
+
+$param = [
+            'image' => 'image/f_id_25.jpg',
+            'sex' => 'F',
+            'description'=>'デニム',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/f_id_26.jpg',
+            'sex' => 'F',
+            'description'=>'ドット柄スカート',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/f_id_27.jpg',
+            'sex' => 'F',
+            'description'=>'マーメイドスカート',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/f_id_28.jpg',
+            'sex' => 'F',
+            'description'=>'ギンガムチェックロングスカート',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/f_id_29.jpg',
+            'sex' => 'F',
+            'description'=>'ドット柄ショートパンツ',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/f_id_30.jpg',
+            'sex' => 'F',
+            'description'=>'黒パンツ',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/f_id_31.jpg',
+            'sex' => 'F',
+            'description'=>'ピンクベージュのライダース',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+        
+$param = [
+            'image' => 'image/id_1.jpg',
+            'sex' => 'M',
+            'description'=>'Ape黒T',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/id_2.jpg',
+            'sex' => 'M',
+            'description'=>'Ape黒T',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/id_3.jpg',
+            'sex' => 'M',
+            'description'=>'Ape白T',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);            
+
+$param = [
+            'image' => 'image/id_4.jpg',
+            'sex' => 'M',
+            'description'=>'Ape白T',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/id_5.jpg',
+            'sex' => 'M',
+            'description'=>'暖色系ボーダーT',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+        
+$param = [
+            'image' => 'image/id_6.jpg',
+            'sex' => 'M',
+            'description'=>'グレーと黒T',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+        
+$param = [
+            'image' => 'image/id_7.jpg',
+            'sex' => 'M',
+            'description'=>'ブルーシャツ',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/id_8.jpg',
+            'sex' => 'M',
+            'description'=>'白ロングスリーブ',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/id_9.jpg',
+            'sex' => 'M',
+            'description'=>'オレンジT',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+        
+$param = [
+            'image' => 'image/id_10.jpg',
+            'sex' => 'M',
+            'description'=>'ジーパン',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+
+$param = [
+            'image' => 'image/id_11.jpg',
+            'sex' => 'M',
+            'description'=>'デニム',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+        
+$param = [
+            'image' => 'image/id_12jpg',
+            'sex' => 'M',
+            'description'=>'ジーパン',
+            'name' => 'tops',
+            ];
+            
+        DB::table('items')->insert($param);
+        
     }
 }
